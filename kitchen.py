e72ce1aacd26c38041127c41d09d43dc012d8cc6
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-06 18:10:22 +0900

    🎨 Update plot over arc (#20)
    * Add extrude rotation contents
    
    * 📝 Update contents of plot over circular arc
    
    * 🎨 Update plot over arc
    
    * Update main.tex

--- kitchen.py
+++ kitchen.py
@@ -3,29 +3,29 @@ from pyvista import examples
 
 pv.set_plot_theme("document")
 
-mesh = examples.load_uniform()
-mesh["height"] = mesh.points[:, 2]
+mesh = examples.download_st_helens()
 
 # Make two points at the bounds of the mesh and one at the center to
 # construct a circular arc.
-normal = [0, 1, 0]
-polar = [mesh.bounds[0], mesh.bounds[2], mesh.bounds[5]]
-center = [mesh.bounds[0], mesh.bounds[2], mesh.bounds[4]]
+normal = [0, 0, 1]
+polar = [4000.0, 0.0, 0.0]
+center = mesh.center
 angle = 90.0
 
 mesh.plot_over_circular_arc_normal(
-    center, 100, normal, polar, angle, "height", fname="velocity.png"
+    center, 10000, normal, polar, angle, fname="velocity.png"
 )
 
 # Preview how this circular arc intersects this mesh
 arc = pv.CircularArcFromNormal(center, 100, normal, polar, angle)
 
 p = pv.Plotter()
-p.add_mesh(mesh, style="wireframe", color="w")
-p.add_mesh(arc, color="b")
+p.add_mesh(mesh)
+p.add_mesh(arc, color="white", line_width=10)
 a = arc.points[0]
 b = arc.points[-1]
 p.add_point_labels(
     [a, b], ["A", "B"], font_size=48, point_color="red", text_color="red"
 )
+p.show_grid()
 p.show(screenshot="kitchen.png")
