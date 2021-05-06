42fd34c901ebc5f9086822c1572b3ad64ec0f7da
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-06 17:44:54 +0900

    🎨 Fix layout (#21)
    * Fix layout
    
    * Fix layout

--- read_file.py
+++ read_file.py
@@ -2,7 +2,7 @@ import pyvista as pv
 
 pv.set_plot_theme("document")
 
-p = pv.Plotter()
+p = pv.Plotter(window_size=[1024, 384])
 mesh = pv.read("bunny.ply")
 p.add_mesh(mesh, color="tan")
 p.camera_position = "xy"
