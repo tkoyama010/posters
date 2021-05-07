fa6fd40e5e3a6b1d7123a482edc848e0643f9900
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-07 18:14:32 +0900

    📝 Update Hello World! content (#23)

--- camera_view.py
+++ camera_view.py
@@ -8,15 +8,15 @@ camera = pv.Camera()
 near_range = 0.3
 far_range = 0.8
 camera.clipping_range = (near_range, far_range)
+
+p = pv.Plotter(window_size=[1024, 384])
+p.camera = camera
+
 unit_vector = np.array(camera.direction) / np.linalg.norm(
     np.array([camera.focal_point]) - np.array([camera.position])
 )
-
 bunny = examples.download_bunny()
 xyz = camera.position + unit_vector * 0.6 - np.mean(bunny.points, axis=0)
 bunny.translate(xyz)
-
-p = pv.Plotter(window_size=[1024, 384])
 p.add_mesh(bunny)
-p.camera = camera
 p.show(screenshot="camera_view.png")
